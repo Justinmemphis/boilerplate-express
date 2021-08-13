b86d866406328ed64b1f7c1cf86ababa1e1c403e
Justin Carter <63558266+Justinmemphis@users.noreply.github.com>
2021-08-13 10:45:32 -0500

    Update myApp.js
    typo

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get("/:word/echo", (req, res) => {
-  const { word } = req.parems;
+  const { word } = req.params;
   res.json({
     echo: word
   });
