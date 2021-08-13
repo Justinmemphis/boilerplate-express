6e54bfde22b6451ca583279434f0cff30d6ba476
Justin Carter <63558266+Justinmemphis@users.noreply.github.com>
2021-08-13 11:07:03 -0500

    Update myApp.js
    typo

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-app.post("/name", function(req, rest) {
+app.post("/name", function(req, res) {
   var string = req.body.first + " " + req.body.last;
   res.json({ name: string });
 });
