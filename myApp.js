14f2ff743bc18b93dc08803971d4d412d4e724d7
Justin Carter <63558266+Justinmemphis@users.noreply.github.com>
2021-08-13 09:31:10 -0500

    Update myApp.js
    some reformatting

--- myApp.js
+++ myApp.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + "/public"));
+
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.use(express.static(__dirname + "/public"));
+
 
 
 
